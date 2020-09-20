d279c6f5a2f1990d2a1c096e6e42b157e47dfef7
MadeYoga <vngla21@gmail.com>
2020-09-20 15:48:49 +0700

    Add tests

---
+++ TestSearch/TestSearchVideo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using YoutubeSearchAPI;
+
+namespace Test
+{
+    class TestSearchVideo
+    { 
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            // Never hardcode your api key...
+            YoutubeSearchClient ytsClient = new YoutubeSearchClient(Environment.GetEnvironmentVariable("DEVELOPER_KEY"));
+
+            dynamic responseObject = await ytsClient.Search("TWICE - Feel Special", maxResults: 1);
+
+            Console.WriteLine(responseObject["items"][0]["id"]["videoId"]);
+        }
+    }
+}
