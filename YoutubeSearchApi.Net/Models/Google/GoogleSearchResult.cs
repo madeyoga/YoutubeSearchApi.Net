e79272bdeee4469b50e65be3f1549989bf13767b
MadeYoga <vngla21@gmail.com>
2021-12-30 23:23:36 +0700

    Add Youtube & Google model

---
+++ YoutubeSearchApi.Net/Models/Google/GoogleSearchResult.cs
@@ -0,0 +1,10 @@
+﻿namespace YoutubeSearchApi.Net.Models.Google;
+
+using System.Collections.Generic;
+
+public class GoogleSearchResult
+{
+    public string Url { get; set; }
+    public string Query { get; set; }
+    public ICollection<GoogleResult> Results { get; set; }
+}
