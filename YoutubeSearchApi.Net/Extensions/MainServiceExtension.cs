731bcfa1d27c2b865180ea5fdd1122d4869d6383
MadeYoga <vngla21@gmail.com>
2022-05-11 21:59:29 +0800

    Add service extension

---
+++ YoutubeSearchApi.Net/Extensions/MainServiceExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using YoutubeSearchApi.Net.Services;
+
+namespace YoutubeSearchApi.Net.Extensions
+{
+    public static class MainServiceExtension
+    {
+        public static void AddYoutubeSearchClient(this IServiceCollection services)
+        {
+            services.AddHttpClient<YoutubeSearchClient>();
+        }
+
+        public static void AddYoutubeMusicSearchClient(this IServiceCollection services)
+        {
+            services.AddHttpClient<YoutubeMusicSearchClient>();
+        }
+    }
+}
